a3f458da637c4debeb1d5bdc88e55014ffa404ef
cho45 <cho45@d0d07461-0603-4401-acd4-de1884942a52>
2007-10-31 04:45:45 +0000

    lang/perl/mobirc/trunk/mobirc/lib/Mobirc/HTTPD/Filter/MailAddress.pm, lang/perl/mobirc/trunk/mobirc/lib/Mobirc/HTTPD/Filter/TelephoneNumber.pm, lang/perl/mobirc/trunk/mobirc/lib/Mobirc/HTTPD/Filter/URL.pm, lang/perl/mobirc/trunk/mobirc/config.yaml.sample:     config.yaml の filter の書き順を変更する必要あり     (URL よりさきに MailAddress, TelephoneNumber)     URI のリンク処理を URL に全部まかせる。     URL より URI という名前のほうがよいかも
        M::F::F::URL は
        accept_schemes に指定したものだけリンクをはる
        指定されてなければ全部
        ついでに redirector が指定されていればそれをリンクの先頭に追記するように
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk@955 d0d07461-0603-4401-acd4-de1884942a52

--- mobirc/lib/Mobirc/HTTPD/Filter/TelephoneNumber.pm
+++ mobirc/lib/Mobirc/HTTPD/Filter/TelephoneNumber.pm
@@ -5,7 +5,7 @@ use warnings;
 sub process {
     my ( $class, $text, $conf ) = @_;
 
-    $text =~ s!\b(0\d{1,3})([-(]?)(\d{2,4})([-)]?)(\d{4})\b!<a href="tel:$1$3$5" class="telephone_number">$1$2$3$4$5</a>!g;
+    $text =~ s!\b(?:tel:)?(0\d{1,3})([-(]?)(\d{2,4})([-)]?)(\d{4})\b!tel:$1$3$5!g;
 
     return $text;
 }
