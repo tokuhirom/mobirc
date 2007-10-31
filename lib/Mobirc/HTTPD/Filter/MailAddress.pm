9037b901418cc75f64e4436d9ecdbdc348d73570
cho45 <cho45@d0d07461-0603-4401-acd4-de1884942a52>
2007-10-31 04:45:45 +0000

    lang/perl/mobirc/trunk/mobirc/lib/Mobirc/HTTPD/Filter/MailAddress.pm, lang/perl/mobirc/trunk/mobirc/lib/Mobirc/HTTPD/Filter/TelephoneNumber.pm, lang/perl/mobirc/trunk/mobirc/lib/Mobirc/HTTPD/Filter/URL.pm, lang/perl/mobirc/trunk/mobirc/config.yaml.sample:     config.yaml の filter の書き順を変更する必要あり     (URL よりさきに MailAddress, TelephoneNumber)     URI のリンク処理を URL に全部まかせる。     URL より URI という名前のほうがよいかも
        M::F::F::URL は
        accept_schemes に指定したものだけリンクをはる
        指定されてなければ全部
        ついでに redirector が指定されていればそれをリンクの先頭に追記するように
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk/mobirc@955 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Mobirc/HTTPD/Filter/MailAddress.pm
+++ lib/Mobirc/HTTPD/Filter/MailAddress.pm
@@ -5,7 +5,7 @@ use warnings;
 sub process {
     my ( $class, $text, $conf ) = @_;
 
-    $text =~ s!\b(\w[\w.+=-]+\@[\w.-]+[\w]\.[\w]{2,4})\b!<a href="mailto:$1" class="mail_address">$1</a>!g;
+    $text =~ s!(?:mailto:)?\b(\w[\w.+=-]+\@[\w.-]+[\w]\.[\w]{2,4})\b!mailto:$1!g;
 
     return $text;
 }
--- lib/Mobirc/HTTPD/Filter/URL.pm
+++ lib/Mobirc/HTTPD/Filter/URL.pm
@@ -1,8 +1,10 @@
 package Mobirc::HTTPD::Filter::URL;
+# vim:expandtab:
 use strict;
 use warnings;
 use URI::Find;
 use URI::Escape;
+@URI::tel::ISA = qw( URI );
 
 sub process {
     my ( $class, $text, $conf ) = @_;
@@ -10,24 +12,44 @@ sub process {
     URI::Find->new(
         sub {
             my ( $uri, $orig_uri ) = @_;
-
-            my $out = qq{<a href="$uri" rel="nofollow" class="url">$orig_uri</a>};
-            if ( $conf->{au_pcsv} ) {
-                $out .=
-                  sprintf( '<a href="device:pcsiteviewer?url=%s" rel="nofollow" class="au_pcsv">[PCSV]</a>',
-                    $uri );
+            if ($conf->{accept_schemes} &&
+                !(grep { $_ eq $uri->scheme } @{ $conf->{accept_schemes} })) {
+                return $orig_uri;
             }
-            if ( $conf->{pocket_hatena} ) {
-                $out .=
-                  sprintf(
-'<a href="http://mgw.hatena.ne.jp/?url=%s&noimage=0&split=1" rel="nofollow" class="pocket_hatena">[ph]</a>',
-                    uri_escape($uri) );
-            }
-            return $out;
+            return (__PACKAGE__->can("process_" . $uri->scheme) ||
+                    \&process_default)->($class, $conf, $uri, $orig_uri);
         }
     )->find( \$text );
 
     return $text;
+}
+
+sub process_http {
+    my ( $class, $conf, $uri, $orig_uri ) = @_;
+    my $out = "";
+    if ( $conf->{redirector} ) {
+        $out = sprintf('<a href="%s%s" rel="nofollow" class="url">%s</a>', $conf->{redirector}, $uri, $uri);
+    } else {
+        $out = qq{<a href="$uri" rel="nofollow" class="url">$orig_uri</a>};
+    }
+    if ( $conf->{au_pcsv} ) {
+        $out .=
+        sprintf( '<a href="device:pcsiteviewer?url=%s" rel="nofollow" class="au_pcsv">[PCSV]</a>',
+            $uri );
+    }
+    if ( $conf->{pocket_hatena} ) {
+        $out .=
+        sprintf(
+            '<a href="http://mgw.hatena.ne.jp/?url=%s&noimage=0&split=1" rel="nofollow" class="pocket_hatena">[ph]</a>',
+            uri_escape($uri) );
+    }
+    return $out;
+}
+
+sub process_default {
+    my ( $class, $conf, $uri, $orig_uri ) = @_;
+    my $out = qq{<a href="$uri" rel="nofollow" class="url">$orig_uri</a>};
+    return $out;
 }
 
 1;
