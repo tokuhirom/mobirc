fe9eb6058afa20bbd7cee41cd96da390b37c1fad
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2007-10-31 15:11:14 +0000

    mobirc: Tiarra Log::Recent parser is now in plugin.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk@996 d0d07461-0603-4401-acd4-de1884942a52

---
+++ mobirc/lib/Mobirc/Plugin/IRCCommand/TiarraLog.pm
@@ -0,0 +1,84 @@
+package Mobirc::Plugin::IRCCommand::TiarraLog;
+use strict;
+use warnings;
+use Encode;
+use Mobirc::Util;
+
+sub register {
+    my ($class, $global_context) = @_;
+
+    $global_context->register_hook(
+        'on_irc_notice' => \&_process,
+    );
+}
+
+sub _process {
+    my ($poe, $who, $channel, $msg) = @_;
+
+    DEBUG "parse tiara's Log::Recent log";
+
+    # Tiarra Log::Recent Parser
+    if ($who && $who eq "tiarra") {
+        # header: %H:%M:%S
+        # header: %H:%M
+        # の場合を想定。後者は Log::Recent のデフォルトだったはず
+        # kick とかに対応していない
+        my $class;
+        my $chann  = encode($poe->heap->{config}->{irc}->{incode}, $channel->[0]);
+        if ($msg =~ qr|^(\d\d:\d\d(?::\d\d)?) ! ([^\s]+?) \((.*)\)|) {
+            # ほんとは quit
+            $class = "part";
+            $who   = $2;
+            $msg   = undef;
+            $chann = $chann;
+        } elsif ($msg =~ qr|^(\d\d:\d\d(?::\d\d)) \+ ([^\s]+?) \(([^\)]+)\) to ([^\s]+)|) {
+            $class = "join";
+            $who   = $2;
+            $msg   = decode("utf8", "$2 join");
+            $chann = $chann;
+        } elsif ($msg =~ qr|^(\d\d:\d\d(?::\d\d)) \- ([^\s]+?) from ([^\s]+)|) {
+            $class = "part";
+            $who   = $2;
+            $msg   = undef;
+            $chann = $chann;
+        } elsif ($msg =~ qr|^(\d\d:\d\d(?::\d\d)) Mode by ([^\s]+?): ([^\s]+) (.*)|) {
+            $class = "mode";
+            $who   = $2;
+            $msg   = undef;
+            $chann = $chann;
+        } elsif ($msg =~ qr|^(\d\d:\d\d(?::\d\d)) Topic of channel ([^\s]+?) by ([^\s]+): (.*)|) {
+            $class = "topic";
+            $who   = $3;
+            $msg   = $4;
+            $chann = $chann;
+        } elsif ($msg =~ qr|^(\d\d:\d\d(?::\d\d)) ([^\s]+?) -> ([^\s]+)|) {
+            $class = "nick";
+            $who   = $3;
+            $msg   = $4;
+            $chann = $chann;
+        } elsif ($msg =~ qr|^(\d\d:\d\d(?::\d\d)) [<>()=-]([^>]+?)[<>()=-] (.*)|) {
+            # priv も notice もまとめて notice に
+            # keyword 反応を再度しないため
+            $class = "notice";
+            $who   = $2;
+            $msg   = $3;
+            $chann = [$chann];
+        }
+
+        if ($class) {
+            DEBUG "RE THROW Tiarra RECENT LOG->$class";
+            $poe->kernel->call(
+                $poe->session,
+                "irc_$class",
+                $who,
+                $chann,
+                encode($poe->heap->{config}->{irc}->{incode}, $msg)
+            );
+            return true;
+        }
+    }
+
+    return false;
+}
+
+1;
