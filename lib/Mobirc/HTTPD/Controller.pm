b20bd60082a8ec398109533dbef2664f013e8605
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2007-10-31 06:20:09 +0000

    lang/perl/mobirc: error handling for invalid xhtml
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk/mobirc@959 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Mobirc/HTTPD/Controller.pm
+++ lib/Mobirc/HTTPD/Controller.pm
@@ -286,16 +286,19 @@ sub _html_filter_docomocss {
         require XML::LibXML;
         require HTML::Selector::XPath;
     };
-    return $content if $@;
+    $@ and return $content;
 
     # escape Numeric character reference.
     $content =~ s/&#(x[\dA-Fa-f]{4}|\d+);/HTMLCSSINLINERESCAPE$1::::::::/g;
+    # unescape Numeric character reference.
+    my $pict_unescape = sub { $content =~ s/HTMLCSSINLINERESCAPE(x[\dA-Z-a-z]{4}|\d+)::::::::/&#$1;/g; return $content; };
 
     $content =~ s{<style type="text/css">(.+)</style>}{}sm;
-    my $css_text = $1 || '';
+    my $css_text = $1 or return $pict_unescape->();
 
     my $css = CSS::Tiny->read_string($css_text);
-    my $doc = XML::LibXML->new->parse_string($content);
+    my $doc = eval { XML::LibXML->new->parse_string($content); };
+    $@ and return $pict_unescape->();
 
     # apply inline css
     while (my($selector, $style) = each %{ $css }) {
@@ -311,10 +314,7 @@ sub _html_filter_docomocss {
 
     $content =~ s{(<a[^>]+)/>}{$1></a>}gi;
 
-    # unescape Numeric character reference.
-    $content =~ s/HTMLCSSINLINERESCAPE(x[\dA-Z-a-z]{4}|\d+)::::::::/&#$1;/g;
-
-    return $content;
+    return $pict_unescape->();
 }
 
 sub set_cookie {
