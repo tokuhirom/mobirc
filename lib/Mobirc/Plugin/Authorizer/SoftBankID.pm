a33445070f4a06fc061ac66f3d9f019e5958a7ab
xcezx <xcezx@d0d07461-0603-4401-acd4-de1884942a52>
2007-11-17 22:51:04 +0000

    ごめんなさいごめんなさいごめんなさいごめんなさい
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk/mobirc@1738 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Mobirc/Plugin/Authorizer/SoftBankID.pm
+++ lib/Mobirc/Plugin/Authorizer/SoftBankID.pm
@@ -1,12 +1,20 @@
-package Mobirc::HTTPD::Authorizer::SoftBankID;
+package Mobirc::Plugin::Authorizer::SoftBankID;
 # vim:expandtab:
 use strict;
 use warnings;
 use Carp;
 use Mobirc::Util;
 
-sub authorize {
-    my ($class, $c, $conf) = @_;
+sub register {
+    my ($class, $global_context, $conf) = @_;
+
+    $global_context->register_hook(
+        'authorize' => sub { my $c = shift;  _authorize($c, $conf) },
+    );
+}
+
+sub _authorize {
+    my ( $c, $conf ) = @_;
 
     unless ($conf->{jphone_uid}) {
         croak "missing jphone_uid; specify your x-jphone-uid string.";
