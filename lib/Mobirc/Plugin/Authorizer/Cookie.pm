575d7fb0be44baacdb196f8509291bcb2e720f45
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-09 01:07:47 +0000

    lang/perl/mobirc: hmm... version related flagged utf8 bug?
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk/mobirc@2929 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Mobirc/Plugin/Authorizer/Cookie.pm
+++ lib/Mobirc/Plugin/Authorizer/Cookie.pm
@@ -5,6 +5,7 @@ use Mobirc::Util;
 use Carp;
 use CGI::Cookie;
 use Digest::MD5 ();
+use Encode;
 
 our $SALT = 'CSS Nite';
 
@@ -51,13 +52,15 @@ sub _set_cookie {
     my ($c, $response, $conf) = @_;
 
     my $password = $conf->{password} or croak "conf->{password} missing";
-
+    my $cookie = CGI::Cookie->new(
+        -name    => 'mobirc_key',
+        -value   => _calc_digest($password),
+        -expires => $conf->{expires} || '+7d',
+    );
+    my $cookie_str = $cookie->as_string;
+    $cookie_str = Encode::is_utf8($cookie_str) ? encode('utf8', $cookie_str) : $cookie_str; ## BK
     $response->push_header(
-        'Set-Cookie' => CGI::Cookie->new(
-            -name    => 'mobirc_key',
-            -value   => _calc_digest($password),
-            -expires => $conf->{expires} || '+7d',
-        )
+        'Set-Cookie' => $cookie_str
     );
 }
 
