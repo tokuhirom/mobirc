f44639f15c69e9b020a3a4796535d2fc34da68b9
kan <kan@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-20 12:42:18 +0000

    mobirc: move input area to index.html
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk/mobirc@5104 d0d07461-0603-4401-acd4-de1884942a52

--- assets/static/mobirc.js
+++ assets/static/mobirc.js
@@ -3,10 +3,12 @@ var load_url;
 function contents_load(url) {
     $('#contents').load(url+'?time='+ts());
     load_url = url;
+    $('#msg').focus();
 }
 function send_message() {
     $.post(load_url, {"msg":($('#msg').get())[0].value}, function (html) {
         $('#contents').html(html);
+        $('#msg').focus();
     });
 }
 setInterval(function(){ if(load_url){ contents_load(load_url); } }, 30000);
