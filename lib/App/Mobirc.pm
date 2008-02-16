7bcf663d420e37bf00d48df0c25e43ea49893270
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-02-16 09:46:16 +0000

    updated coverage rate
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk@6781 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/Mobirc.pm
+++ lib/App/Mobirc.pm
@@ -128,29 +128,29 @@ I use Devel::Cover to test the code coverage of my tests, below is the Devel::Co
     ---------------------------- ------ ------ ------ ------ ------ ------ ------
     File                           stmt   bran   cond    sub    pod   time  total
     ---------------------------- ------ ------ ------ ------ ------ ------ ------
-    blib/lib/App/Mobirc.pm         80.0   35.7   62.5   87.0    0.0   10.5   66.5
-    ...lib/App/Mobirc/Channel.pm   77.3   53.6   66.7   81.2   14.3    1.7   68.3
-    ...pp/Mobirc/ConfigLoader.pm   94.3   50.0   72.7  100.0    0.0   24.4   81.8
-    blib/lib/App/Mobirc/HTTPD.pm   63.3    0.0    0.0   85.2    0.0    2.9   56.4
-    ...obirc/HTTPD/Controller.pm   38.9   19.4    0.0   61.3    0.0    2.7   35.6
-    ...pp/Mobirc/HTTPD/Router.pm   40.9    0.0    n/a   85.7    0.0    5.5   30.0
-    ...lib/App/Mobirc/Message.pm  100.0    n/a    n/a  100.0  100.0    0.7  100.0
-    ...n/Authorizer/BasicAuth.pm   48.0    0.0    0.0   57.1    0.0    0.1   38.1
-    ...ugin/Authorizer/Cookie.pm   44.7    0.0    0.0   53.8    0.0    0.1   37.8
-    ...horizer/EZSubscriberID.pm   48.0    0.0    0.0   57.1    0.0    0.1   40.0
+    blib/lib/App/Mobirc.pm         80.0   35.7   62.5   87.0    0.0    3.0   66.5
+    ...lib/App/Mobirc/Channel.pm   77.3   53.6   66.7   81.2   14.3    0.7   68.3
+    ...pp/Mobirc/ConfigLoader.pm   94.3   50.0   72.7  100.0    0.0    8.2   81.8
+    blib/lib/App/Mobirc/HTTPD.pm   63.3    0.0    0.0   85.2    0.0    1.1   56.4
+    ...obirc/HTTPD/Controller.pm   38.9   19.4    0.0   61.3    0.0    1.0   35.6
+    ...pp/Mobirc/HTTPD/Router.pm   40.9    0.0    n/a   85.7    0.0    0.1   30.0
+    ...lib/App/Mobirc/Message.pm  100.0    n/a    n/a  100.0  100.0    0.3  100.0
+    ...n/Authorizer/BasicAuth.pm   48.0    0.0    0.0   57.1    0.0    0.0   38.1
+    ...ugin/Authorizer/Cookie.pm   44.7    0.0    0.0   53.8    0.0    0.0   37.8
+    ...horizer/EZSubscriberID.pm   48.0    0.0    0.0   57.1    0.0    0.0   40.0
     .../Authorizer/SoftBankID.pm   52.2    0.0    0.0   57.1    0.0    0.0   42.1
-    ...in/Component/IRCClient.pm   83.2   33.3   41.7   83.3    0.0    8.3   72.1
-    .../Mobirc/Plugin/DocRoot.pm   80.6   25.0    n/a   75.0    0.0    2.8   69.8
-    ...PS/InvGeocoder/EkiData.pm   93.8   75.0    n/a  100.0    0.0    3.9   90.9
-    ...S/InvGeocoder/Nishioka.pm   93.3   50.0    n/a  100.0    0.0    1.8   90.0
-    ...TMLFilter/CompressHTML.pm   90.9    n/a    n/a   80.0    0.0    0.2   85.7
-    ...lter/ConvertPictograms.pm   84.6    n/a    n/a   80.0    0.0    0.8   78.9
-    ...n/HTMLFilter/DoCoMoCSS.pm   37.5    0.0    n/a   66.7    0.0    0.2   35.3
-    ...n/IRCCommand/TiarraLog.pm   21.4    0.0    0.0   57.1    0.0    0.1   18.4
-    ...geBodyFilter/Clickable.pm   92.8   83.3   72.7   83.3    0.0    8.7   85.1
-    ...ageBodyFilter/IRCColor.pm   77.3   64.3  100.0   81.8    0.0    0.9   72.4
-    blib/lib/App/Mobirc/Util.pm   100.0   50.0    n/a  100.0    0.0   23.8   86.3
-    Total                          66.6   32.1   36.0   78.1    2.8  100.0   59.1
+    ...in/Component/IRCClient.pm   83.2   33.3   41.7   83.3    0.0    4.7   72.1
+    .../Mobirc/Plugin/DocRoot.pm   80.6   25.0    n/a   75.0    0.0   10.0   69.8
+    ...PS/InvGeocoder/EkiData.pm   93.8   75.0    n/a  100.0    0.0    1.0   90.9
+    ...S/InvGeocoder/Nishioka.pm   96.7   50.0    n/a  100.0    0.0    0.6   92.5
+    ...TMLFilter/CompressHTML.pm   90.9    n/a    n/a   80.0    0.0    0.1   85.7
+    ...lter/ConvertPictograms.pm   84.6    n/a    n/a   80.0    0.0    0.3   78.9
+    ...n/HTMLFilter/DoCoMoCSS.pm  100.0   50.0    n/a  100.0    0.0   65.7   91.2
+    ...n/IRCCommand/TiarraLog.pm   21.4    0.0    0.0   57.1    0.0    0.0   18.4
+    ...geBodyFilter/Clickable.pm   92.8   83.3   72.7   83.3    0.0    1.3   85.1
+    ...ageBodyFilter/IRCColor.pm   87.9   81.0  100.0   81.8    0.0    0.7   83.7
+    blib/lib/App/Mobirc/Util.pm   100.0   50.0    n/a  100.0    0.0    1.1   86.3
+    Total                          69.6   36.1   36.0   79.2    2.8  100.0   61.8
     ---------------------------- ------ ------ ------ ------ ------ ------ ------
 
 =head1 AUTHOR
