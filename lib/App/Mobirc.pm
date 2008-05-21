680bd70d8952000002984b944cff689a194ead67
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-21 23:37:49 +0000

    split pluggable stuff from core.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk@12179 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/Mobirc.pm
+++ lib/App/Mobirc.pm
@@ -1,5 +1,6 @@
 package App::Mobirc;
 use Moose;
+with 'App::Mobirc::Role::Pluggable';
 use 5.00800;
 use Scalar::Util qw/blessed/;
 use POE;
@@ -12,12 +13,6 @@ use App::Mobirc::Model::Server;
 use Encode;
 
 our $VERSION = '0.06';
-
-our $HasKwalify;
-eval {
-    require Kwalify;
-    $HasKwalify++;
-};
 
 has server => (
     is      => 'ro',
@@ -47,25 +42,6 @@ around 'new' => sub {
     return $self;
 };
 
-sub load_plugins {
-    my ($self,) = @_;
-    die "this is instance method" unless blessed $self;
-
-    for my $plugin (@{$self->config->{plugin}}) {
-        DEBUG "LOAD PLUGIN: $plugin->{module}";
-        $plugin->{module}->use or die $@;
-        if ( $HasKwalify && $plugin->{module}->can('config_schema') ) {
-            my $res = Kwalify::validate( $plugin->{module}->config_schema, $plugin->{config} );
-            unless ( $res == 1 ) {
-                die "config.yaml validation error : $plugin->{module}, $res";
-            }
-        }
-        $plugin->{module}->register( $self, $plugin->{config} );
-    }
-}
-
-sub config { shift->{config} }
-
 sub run {
     my $self = shift;
     die "this is instance method" unless blessed $self;
@@ -77,21 +53,6 @@ sub run {
     App::Mobirc::HTTPD->init($self->config);
 
     $poe_kernel->run();
-}
-
-sub register_hook {
-    my ($self, $hook_point, $code) = @_;
-    die "this is instance method" unless blessed $self;
-    croak "code required" unless ref $code eq 'CODE';
-
-    push @{$self->{hooks}->{$hook_point}}, $code;
-}
-
-sub get_hook_codes {
-    my ($self, $hook_point) = @_;
-    die "this is instance method" unless blessed $self;
-    croak "hook point missing" unless $hook_point;
-    return $self->{hooks}->{$hook_point} || [];
 }
 
 1;
