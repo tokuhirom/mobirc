bf082181ea252a8387b2668176bfed99811c8b8d
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-06-15 03:33:24 +0000

    fixed broken link
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk@14012 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/Mobirc/Web/Template/Parts.pm
+++ lib/App/Mobirc/Web/Template/Parts.pm
@@ -12,7 +12,7 @@ template 'keyword_line' => sub {
     my ($self, $row, $irc_nick) = @_;
     show 'irc_message', $row, $irc_nick;
     outs '(';
-        a { attr { 'href' => '/mobile/channel?channel=' . uri_escape( $row->channel->name) }
+        a { attr { 'href' => '/mobile/channel?channel=' . $row->channel->name_urlsafe_encoded }
             $row->channel->name
         };
     outs ')';
