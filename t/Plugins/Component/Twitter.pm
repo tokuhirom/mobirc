67c172ee3d7fee27c335452c3e9f88c29f9a0d51
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-02 13:36:22 +0000

    lang/perl/mobirc: Checking in changes prior to tagging of version 1.07.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 29036)
    +++ Changes	(local)
    @@ -1,3 +1,10 @@
    +1.07
    +
    +    - change authentication flow
    +    - use HTTP::Session
    +    - Plugin::Component::Twitter: deprecated
    +    - better iphone mode(retlet++)
    +
     1.06
     
         - bump up deps to PoCoIRC 5.88
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/mobirc/trunk@22586 d0d07461-0603-4401-acd4-de1884942a52

--- t/Plugins/Component/Twitter.pm
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-use Test::More;
-eval "use POE::Component::Client::Twitter";
-plan skip_all => "this tests requires PoCo::C::Twitter" if $@;
-plan tests => 1;
-
-use_ok 'App::Mobirc::Plugin::Component::Twitter';
-
