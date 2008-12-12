3d9aa04bd2a4cf7fcbaa2900d44a78360574130a
Tokuhiro Matsuno <tokuhirom@gp.ath.cx>
2008-12-13 00:52:37 +0900

    added debugging module

---
+++ lib/App/Mobirc/Plugin/Debug.pm
@@ -0,0 +1,17 @@
+package App::Mobirc::Plugin::Debug;
+use MooseX::Plaggerize::Plugin;
+use GTop;
+use utf8;
+
+my $gtop = GTop->new;
+warn "LOAD";
+
+hook process_command => sub {
+    my ( $self, $global_context, $command, $channel ) = @_;
+    if ($command =~ /!dan/) {
+        printf "Process size : %d\n", $gtop->proc_mem($$)->size;
+    }
+    return 0;
+};
+
+1;
