ba9aa72442a47e16c961f16671814066b115c8c9
Tokuhiro Matsuno <tokuhirom@gp.ath.cx>
2008-12-15 19:10:15 +0900

    feature enhancement for debug module :)

--- lib/App/Mobirc/Plugin/Debug.pm
+++ lib/App/Mobirc/Plugin/Debug.pm
@@ -1,18 +1,31 @@
 package App::Mobirc::Plugin::Debug;
 use strict;
 use App::Mobirc::Plugin;
-use GTop;
 use utf8;
-
-my $gtop = GTop->new;
-warn "LOAD";
 
 hook process_command => sub {
     my ( $self, $global_context, $command, $channel ) = @_;
-    if ($command =~ /!dan/) {
-        printf "Process size : %d\n", $gtop->proc_mem($$)->size;
+    if (my ($command, ) = ( $command =~ /^!(\S+)/ )) {
+        my $meth = "do_$command";
+        __PACKAGE__->$meth;
     }
     return 0;
 };
+
+sub do_memory {
+    require GTop;
+    my $gtop = GTop->new;
+    printf "Process size : %d\n", $gtop->proc_mem($$)->size;
+}
+
+sub do_dumpinc {
+    require Data::Dumper;
+    warn Data::Dumper::Dumper(\%INC);
+}
+
+sub do_reload {
+    require Module::Reload;
+    Module::Reload->check;
+}
 
 1;
