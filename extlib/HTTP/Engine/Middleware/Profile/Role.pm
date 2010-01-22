1970899a246a8fc3cbfb28bfbb8572dbe34694ad
tokuhirom <tokuhirom@gmail.com>
2010-01-23 06:22:17 +0900

    remove HTTP::Engine, and add install Plack.

--- extlib/HTTP/Engine/Middleware/Profile/Role.pm
+++
@@ -1,8 +0,0 @@
-package HTTP::Engine::Middleware::Profile::Role;
-use Any::Moose '::Role';
-
-requires 'start';
-requires 'end';
-requires 'report';
-
-1;
--- extlib/HTTP/Engine/Role/ResponseWriter.pm
+++
@@ -1,7 +0,0 @@
-package HTTP::Engine::Role::ResponseWriter;
-use Any::Moose '::Role';
-
-requires qw(finalize);
-
-1;
-
